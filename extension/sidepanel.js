93d003ee48814ab781b6f3cdf06a89a94c844e18
bpiper02 <93992231+bpiper02@users.noreply.github.com>
2026-09-11 12:12:05 -0400

    Fix Assist HTML escaping after dashboard sync patch

--- extension/sidepanel.js
+++ extension/sidepanel.js
@@ -14,7 +14,7 @@
 
   function escapeHtml(value) {
     return String(value ?? '').replace(/[&<>"']/g, char => ({
-      '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#039;'
+      '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'
     }[char]));
   }
 
